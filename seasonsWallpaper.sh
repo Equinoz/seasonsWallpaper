71006e12a5e4a8b83eeae3e27dd0e4b9855022f3
Equinoz <julien.lecoz33@gmail.com>
2020-08-13 12:15:23 +0200

    Plan

---
+++ seasonsWallpaper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Récupérer la date et déterminer la saison courante
+# Indiquer le fichier où se trouve les fonds d'écran en fonction de la saison
+# Choisir un fond d'écran au hasard
+# Modifier le fond d'écran actuel
+# Va falloir faire une variable qui représentera le dossier où se trouve les 4 dossiers de fonds d'écran
