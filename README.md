3b813da3eb6ef28923b236420596fef6d8fe65db
Equinoz <julien.lecoz33@gmail.com>
2020-08-13 18:39:37 +0200

    Ajout de la gestion des dossiers manquants, du gitignore et modification du README.md

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Seasons Wallpaper
-### Script bash permettant de modifier le fond d'écran en fonction des saisons sous environnement de bureau XFCE
+### Script bash pour Linux sous environnement de bureau XFCE, permettant de modifier le fond d'écran en fonction des saisons
+
+___
+
+##### La constante DIRECTORY_PATH doit être définie dans le script afin d'indiquer le chemin absolu du dossier où se trouvent les fonds d'écran. Dans ce dossier, les images doivent se trouver dans quatre sous-dossiers différents nommés "winter", "spring", "summer" et "autumn"
+
+##### Utilisez crontab pour lancer le script périodiquement. Lancez la commande:
+```bash
+crontab -e
+```
+##### puis à la fin de ce fichier ajoutez par exemple la ligne:
+```bash
+* * */5 * * bash /chemin_absolu_du_script/seasonsWallpaper.sh
+```
+##### pour lancer le script tout les 5 jours
--- seasonsWallpaper.sh
+++ seasonsWallpaper.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+#----------------------------------------------------------------------------------#
+#                                                                                  #
+#  For use the script, the DIRECTORY_PATH constant must points to a directory      #
+#  containing four others directories. These directories' names must be "winter",  #
+#  "spring", "summer" and "autumn" and must contain wallpapers to display          #
+#                                                                                  #
+#----------------------------------------------------------------------------------#
+
+# Define the absolute directory path that contains the four others directories
+DIRECTORY_PATH=/home/julien/Bureau/seasonsWallpaper/wallpapers
+
+# Check the required directories
+if [ ! -d $DIRECTORY_PATH ]
+	then echo "Error: specified directory \"$DIRECTORY_PATH\" doesn't exist"
+	exit
+fi
+
+for directory in "winter" "spring" "summer" "autumn"
+do
+	if [ ! -d $DIRECTORY_PATH/$directory ]
+		then echo "Error: there is no \"$directory\" directory in $DIRECTORY_PATH"
+			exit
+	fi
+done
 
 # Get the date to determinate the current season
 month=`date "+%m"`
@@ -17,9 +41,9 @@ if [ $month = 01 ] || [ $month = 02 ] || ( [ $month = 12 ] && [ $day -gt 15 ] ) 
 		season="autumn"
 fi
 
-echo $season
+wallpapersPath=$DIRECTORY_PATH/$season
 
-# Indiquer le fichier où se trouve les fonds d'écran en fonction de la saison
-# Choisir un fond d'écran au hasard
+echo $wallpapersPath
+
+# Choisir un fond d'écran au hasard dans le dossier correspondant à la saison en cours
 # Modifier le fond d'écran actuel
-# Va falloir faire une variable qui représentera le dossier où se trouve les 4 dossiers de fonds d'écran
